dfcdbf25d4910eaec839b26285edb28d0f91a33b
Ajay Pandit <ajayoneness123@gmail.com>
2024-07-21 23:09:20 +0530

    newjnfdjnfkd fdmmf

--- pythoncoding4u/settings.py
+++ pythoncoding4u/settings.py
@@ -144,19 +144,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
-MEDIA_URL = '/media/'
-
-
-# if DEBUG:
-#     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-#
-# else:
-#     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+
+
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 
 
